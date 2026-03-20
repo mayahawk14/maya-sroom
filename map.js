0cd4f35330e5c1a1e16d0ff689c6f0b75ab36b89
Maya Hawkins <59785033+mayahawk14@users.noreply.github.com>
2026-03-20 01:11:23 -0500

    Add slideshow functionality in map.js

---
+++ map.js
@@ -0,0 +1,31 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 0}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
